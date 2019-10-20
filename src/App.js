4d01e44e22172ef23b70420571a5b8262d3b0829
nutbum05 <lukasz.orzech@gmail.com>
2019-10-20 21:01:35 +0100

    created single page app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card key={profile.id} {...profile} />)};
+</div>
+);
+
+class Card extends React.Component {
+  render() {
+    const profie = this.props
+    return (
+      <div className="github-profile">
+        <img src={profie.avatar_url} alt='user'/>
+        <div className="info">
+          <div className="name">{profie.name}</div>
+          <div className="company">{profie.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  state = { userName: '' }
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await
+      axios.get(`https://api.github.com/users/${this.state.userName}`);
+      this.props.onSubmit(resp.data)
+      this.setState({userName: ''})
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          value={this.state.userName}
+          onChange={event => this.setState({ userName: event.target.value })}
+          placeholder='GtiHub username'
+          required />
+        <button>Add card</button>
+      </form>
+    );
+  }
+
+}
+
+class App extends React.Component {
+  state = {
+      profiles: []
+    };
+    addNewProfile = (profileData) => {
+this.setState(prevState => ({
+profiles: [...prevState.profiles, profileData]
+}))
+
+    };
+  
+
+
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
 }
 
 export default App;
